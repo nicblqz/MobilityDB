49f695960c1f945fe8bb4321154256759a28fc2c
nblqz <nicolas.blanquez-yeste@ulb.be>
2024-04-26 17:14:57 +0200

    test, 2 points

--- meos/PROJET/struct.h
+++ meos/PROJET/struct.h
@@ -21,24 +21,3 @@ typedef struct {
     Trips* trips;
     Trips* uncompressed_trips;
 } BWC_DR;
-
-/*class BWC_DR {
-    public:
-        int total;
-        int limit;
-        int window;
-        int start;
-        Trips *trips;
-        // priority_list priority_list;
-        Trips *uncompressed_trips;
-        // priority_list* finished_windows;
-        void set_limit(int limit){
-            this->limit = limit;
-        }
-        void set_window(int window){
-            this->window = window;
-        }
-        void set_start(int start){
-            this->start = start;
-        }
-};*/
--- meos/PROJET/test.c
+++ meos/PROJET/test.c
@@ -6,27 +6,53 @@
 int main()
 {
     meos_initialize(NULL, NULL);
-    char *inst_wkt = "POINT(1 1)@2000-01-01";
-    PPoint *ppoint = (PPoint *) malloc(sizeof(PPoint));
-    ppoint->tid = 1;
-    ppoint->point = tgeompoint_in(inst_wkt);
+
     BWC_DR *bwc;
     bwc = (BWC_DR *) malloc(sizeof(BWC_DR));
-    bwc->trips = (Trips *) malloc(sizeof(Trips));
-    bwc->trips->id = ppoint->tid;
-    bwc->trips->ppoints = ppoint;
-    char *inst_mfjson = temporal_as_mfjson(bwc->trips->ppoints->point, true, 3, 6, "EPSG:4326");
+
+    char *inst_1 = "POINT(1 1)@2000-01-01";
+    char *inst_2 = "POINT(2 2)@2000-01-02";
+    bwc->total = 2;
+
+    PPoint *ppoint1 = (PPoint *) malloc(sizeof(PPoint));
+    ppoint1->tid = 1;
+    ppoint1->point = tgeompoint_in(inst_1);
+    ppoint1->priority = 1;
+    PPoint *ppoint2 = (PPoint *) malloc(sizeof(PPoint));
+    ppoint2->tid = 2;
+    ppoint2->point = tgeompoint_in(inst_2);
+    ppoint2->priority = 2;
+
+    Trips *trip1 = (Trips *) malloc(sizeof(Trips));
+    PPoint ppoints[bwc->total];
+    ppoints[0] = *ppoint1;
+    ppoints[1] = *ppoint2;
+    trip1->id = 1;
+    trip1->ppoints = ppoints;
+    bwc->trips = trip1;
+    
+    char *inst_mfjson1 = temporal_as_mfjson(bwc->trips->ppoints[ppoint1->tid-1].point, true, 3, 6, "EPSG:4326");
+    char *inst_mfjson2 = temporal_as_mfjson(bwc->trips->ppoints[ppoint2->tid-1].point, true, 3, 6, "EPSG:4326");
     printf("\n"
     "--------------------\n"
-    "| Temporal Instant |\n"
+    "| Temporal Instant 1|\n"
     "--------------------\n\n"
     "WKT:\n"
     "----\n%s\n\n"
     "MF-JSON:\n"
-    "--------\n%s\n", inst_wkt, inst_mfjson);
+    "--------\n%s\n", inst_1, inst_mfjson1);
+    printf("\n"
+    "--------------------\n"
+    "| Temporal Instant 2|\n"
+    "--------------------\n\n"
+    "WKT:\n"
+    "----\n%s\n\n"
+    "MF-JSON:\n"
+    "--------\n%s\n", inst_2, inst_mfjson2);
     free(bwc);
     free(bwc->trips);
-    free(ppoint->point);
+    free(ppoint1->point);
+    free(ppoint2->point);
     meos_finalize();
     return 0;
 }
